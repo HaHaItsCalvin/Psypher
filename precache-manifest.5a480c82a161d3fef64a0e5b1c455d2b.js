e989e9f5e550c827b5248cff4ed425ddf93dfddb
Calvin <calvinha721@gmail.com>
2018-10-15 23:54:16 -0700

    Updates

---
+++ precache-manifest.5a480c82a161d3fef64a0e5b1c455d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab31dc494e3823104241042435101db",
+    "url": "/StealthxFox/static/media/Corgeek.4ab31dc4.jpg"
+  },
+  {
+    "revision": "37d85b6dfb498abd30d5",
+    "url": "/StealthxFox/static/js/runtime~main.37d85b6d.js"
+  },
+  {
+    "revision": "aaf10ff6e3970dcfd33b",
+    "url": "/StealthxFox/static/js/main.aaf10ff6.chunk.js"
+  },
+  {
+    "revision": "a481d2ae6377be0cc4ab",
+    "url": "/StealthxFox/static/js/1.a481d2ae.chunk.js"
+  },
+  {
+    "revision": "aaf10ff6e3970dcfd33b",
+    "url": "/StealthxFox/static/css/main.cde26b9e.chunk.css"
+  },
+  {
+    "revision": "7142841b9afd78819155971d002362f7",
+    "url": "/StealthxFox/index.html"
+  }
+];
